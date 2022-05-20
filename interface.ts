57f28662acc7491587a42d4fac719ef198dce83b
Juan García <43949658+garcia-s@users.noreply.github.com>
2022-05-20 16:37:56 -0500

    initial commit

---
+++ interface.ts
@@ -0,0 +1,45 @@
+export interface ITransportServer {
+    removeClient(client: ITransportClient): void;
+    //Returns a list of the Transport clients
+    get clients(): ITransportClient[];
+    // Should register the connection listener in the transport implementation
+    onConnect(listener: (socket: ITransportClient) => void): void;
+    // Should register the error listener in the transport implementation
+    onError(listener: (error: Error) => void): void;
+    // Should register the close listener  in the transport implementation
+    onClose(listener: () => void): void;
+  }
+  
+  export enum TransportClientState {
+    CONNECTING = 0,
+    OPEN = 1,
+    CLOSING = 2,
+    CLOSED = 3,
+  }
+  
+  export interface ITransportClient {
+    get rooms(): string[];
+  
+    get state(): TransportClientState;
+    //Adds a listener to the list
+    on(event: string, listener: (data: any) => void): void;
+    //Removes a listener from the list
+    off(event: string, listener: () => void): void;
+    //Adds a self removing listener that executes only once
+    once(event: string, listener: (data: any) => void): void;
+    //emitted when the socket receives a close request
+    onClose(listener: () => void): void;
+    //emitted when an error has ocurred in the socket client
+    onError(listener: (event: any) => void): void;
+    //Sends an event to the client socket
+    emit(event: string, data: any, onError?: (error: Error) => void): void;
+    //Sends an event to all clients in the server;
+    broadcast(event: string, data?: any): void;
+    //Sends an event to all clients in a room
+    emitToRoom(room: string, event: string, data?: any): void;
+    //Enters a room
+    join(room: string): void;
+    //Leaves a room
+    leave(room: string): void;
+  }
+  
